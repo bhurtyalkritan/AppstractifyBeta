6e2c507fe31ea57f75cafaaf9a9865cebe428cd4
Kritan Bhurtyal <66888896+Lukasthefotto@users.noreply.github.com>
2023-10-12 10:39:46 -0400

    Update synthetic.py

--- synthetic.py
+++ synthetic.py
@@ -1,36 +1,45 @@
-#from langchain import OpenAI
-#from langchain import prompts
+from langchain.llms import OpenAI
+from langchain import prompts
 import pandas as pd
-import faker
+from faker import Faker
 
+global Earthquake_df
+global Employee_df
+global Disease_df
+global Econ_df
+global Healthcare_df
+global Education_df
+global Flight_df
 
-# declaring template values
 Earthquake_df = pd.read_csv("templates/Earthquake_Data.csv")
 Employee_df = pd.read_csv("templates/EmployeeData.csv")
 Disease_df = pd.read_csv("templates/Disease.csv")
-Econ_df = ""
-Healthcare_df = ""
-Education_df = ""
-Population_df = ""
-Machine_df = ""
-Research_df = ""
-Flight_df = ""
 
-
-def export_dataframe(data):
+def export_dataframe(data,format):
     df = data
-    df_csv = df.to_csv('blank.csv',index=True)
+    df_csv = df.to_csv('blank.csv', index=True)
     print(df_csv)
 
+def excel_converter(df):
+    df = pd.read_csv(df)
+    df = df.to_excel("download.xlsx",sheet_name="Download",index=False)
+    print(df)
+def csv_converter():
+    return
+def prompt_gpt(user_input,key):
+    try:
+        llm = OpenAI(openai_api_key=key,temperature=0.9)
+        df = llm.predict(user_input)
+        return df
+    except:
+        print("Rate exceeded or bug found")
 
-def prompt_gpt(user_input):
-    #llm = OpenAI.llm
-
-def prompt_bard():
-    pass
-
+def prompt_bard(query):
+    API_KEY = "AIzaSyBAKeIaKuCO6sEQvFQyJ8Az18PfDEuMwl4"
+    df = google_bard.generate_text(query, api_key=API_KEY)
+    return df
 def prompt_llama():
-    pass
+    return
 
 def faker_lib():
-    pass
+    return
